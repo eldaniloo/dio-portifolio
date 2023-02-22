42c31776ca4d76e411d66f9ff1d69adb6f6070a5
eldaniloo <108435825+eldaniloo@users.noreply.github.com>
2023-02-22 02:47:29 -0300

    Update api.js
    atualizando links da api

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,7 @@
 
 
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/eldaniloo/dio-portifolio/main/dio-portfolio/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/eldaniloo/dio-portifolio/main/data/profile.json';
     const fetching = await fetch(url)
     return await fetching.json()
 }

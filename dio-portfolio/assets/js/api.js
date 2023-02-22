33711b11893155ef4e8bced41a78a81542423608
eldaniloo <108435825+eldaniloo@users.noreply.github.com>
2023-02-22 02:41:50 -0300

    Delete dio-portfolio directory
    excluindo arquivos

--- dio-portfolio/assets/js/api.js
+++
@@ -1,7 +0,0 @@
-
-
-async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/eldaniloo/dio-portifolio/main/dio-portfolio/data/profile.json';
-    const fetching = await fetch(url)
-    return await fetching.json()
-}
